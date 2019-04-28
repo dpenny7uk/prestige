0dd3b75d0a450a5710efb9d1110cdbdad3cd1cab
dpenny7uk <dpenny7uk@hotmail.com>
2019-04-28 18:39:54 +0100

    Add files via upload

---
+++ public/test.js
@@ -0,0 +1,19 @@
+// class Appointment {
+//   constructor (firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+//   book() {
+//     return 'Thanks for booking your appointment ' + this.firstName + ' ' +  this.lastName
+//   }
+// }
+
+// class FittingAppointment extends Appointment {
+
+// }
+
+
+
+
+// const damianBooking = new Appointment ('Damian', 'Penny')
+// damianBooking.book()
